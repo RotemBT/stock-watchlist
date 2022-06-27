66b3a00817371f8f5b6df5ea3f2225f5a583899f
rotembt <rotem001@gmail.com>
2022-06-27 20:44:02 +0300

    add redis

--- backend-service/main.py
+++ backend-service/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from alpaca_trade_api.rest import REST, TimeFrame
 import config
+import alpaca_trade_api
 from typing import List
-
+import redis
+alpacaAPI = alpaca_trade_api.rest
 app = FastAPI()
+redis = redis.Redis(host='localhost', port=6379, db=0)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,9 +17,11 @@ app.add_middleware(
 )
 stockWatch: List[str] = []
 
+
 @app.get("/")
 def root():
-    return {'hello':'world'}
+    return {'hello': 'world'}
+
 
 @app.get('/api/news')
 async def get_stocks_list(stocks: str):
@@ -25,19 +29,25 @@ async def get_stocks_list(stocks: str):
     stocksList = stocks.split(',')
     stockWatch.extend(stocksList)
     stockWatch = list(set(stockWatch))
-    rest_client = REST(config.API_KEY, config.SECRET_KEY)
-    news = rest_client.get_news(stocks, config.NEWS_START_DATE, config.NEWS_END_DATE)
+    rest_client = alpacaAPI.REST(config.API_KEY, config.SECRET_KEY)
+    news = rest_client.get_news(
+        stocks, config.NEWS_START_DATE, config.NEWS_END_DATE)
     newsFormatted = []
     for n in news:
         newsFormatted.append(n._raw)
     return {'news': newsFormatted}
-    
+
+
 @app.get('/api/bars')
 async def get_stocks_list(stock: str):
-    rest_client = REST(config.API_KEY, config.SECRET_KEY)
-    data = rest_client.get_bars_iter(stock, TimeFrame.Day, config.BARS_START_DATE, config.BARS_END_DATE, adjustment='raw')
+    cachedBars = redis.get(stock)
+    if cachedBars:
+        return {stock: cachedBars}
+    rest_client = alpacaAPI.REST(config.API_KEY, config.SECRET_KEY)
+    data = rest_client.get_bars_iter(
+        stock, alpacaAPI.TimeFrame.Day, config.BARS_START_DATE, config.BARS_END_DATE, adjustment='raw')
     barsFormatted = []
     for n in data:
         barsFormatted.append(n._raw)
+    redis.set(stock, barsFormatted)
     return {stock: barsFormatted}
-
